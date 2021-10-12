92cc1f2c8f981c25828d30be504b013406d2d6ea
201601972-kimdongwook <62870938+201601972-kimdongwook@users.noreply.github.com>
2021-10-12 20:17:07 +0900

    1-6 조건부 렌더링

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function Hello({ color, name }) { // 비구조화 할당
-    return <div style={{ color }}>hello {name}!</div>
+function Hello({ color, name, isSpecial }) { // 비구조화 할당
+    return (
+        <div style={{ color }}>
+            {isSpecial && <b>*</b>} {/*단축 평가 논리 계산법*/}
+            hello {name}!
+        </div>
+    )
 }
 
 // Hello 컴포넌트의 기본 props 설정
